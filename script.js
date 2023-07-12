f7be412945f23e021a1dd040e7a4e1fa7842add4
4A-61-73-70-65-72 <139377723+4A-61-73-70-65-72@users.noreply.github.com>
2023-07-12 12:56:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+function loadFile(event, inputId) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            let text = e.target.result;
+            text = text.replace(/\s+/g, ' ').trim();
+            document.getElementById(inputId).value = text;
+        };
+        reader.readAsText(file);
+    }
+}
+
+function next() {
+    document.getElementById('screen1').classList.add('hidden');
+    document.getElementById('screen2').classList.remove('hidden');
+}
+
+function compareHashes() {
+    const input1 = document.getElementById('input1').value;
+    const input2 = document.getElementById('input2').value;
+    const hash1 = CryptoJS.SHA256(input1);
+    const hash2 = CryptoJS.SHA256(input2);
+    
+    if (hash1.toString() === hash2.toString()) {
+        document.getElementById('output').innerText = "Hashes are the same: " + hash1;
+    } else {
+        document.getElementById('output').innerText = "Hashes are different: \nHash 1: " + hash1 + "\nHash 2: " + hash2;
+    }
+}
+
+function reset() {
+    document.getElementById('input1').value = '';
+    document.getElementById('input2').value = '';
+    document.getElementById('output').innerText = '';
+    document.getElementById('screen1').classList.remove('hidden');
+    document.getElementById('screen2').classList.add('hidden');
+}
